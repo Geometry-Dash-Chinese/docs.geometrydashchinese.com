43298b2fa2d6eacee89e3902dec21f71f1acec99
渣渣120 <WOSHIZHAZHA120@qq.com>
2022-08-21 17:20:35 +0800

    initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,49 @@
+export default {
+    themeConfig: {
+        siteTitle: '几何冲刺文档站',
+        sidebar: [
+            {
+                text: 'GDCS',
+                items: [
+                    {
+                        text: '指令',
+                        items: [
+                            {
+                                text: '语法',
+                                link: '/gdcs/game/command/inspect'
+                            },
+                            {
+                                text: '账号命令',
+                                items: [
+                                    {
+                                        text: '',
+                                        items: [
+                                            {
+                                                text: '!test',
+                                                link: '/gdcs/game/command/common/test'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                text: '关卡命令',
+                                items: [
+                                    {
+                                        text: '',
+                                        items: [
+                                            {
+                                                text: '!test',
+                                                link: '/gdcs/game/command/common/test'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
